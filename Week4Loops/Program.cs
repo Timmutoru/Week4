7f71572c51e517f8ee3a2655dae5266c59f0a3fc
Timmutoru <124074292+Timmutoru@users.noreply.github.com>
2023-05-09 18:42:23 +0300

    Add files via upload

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,13 @@
+﻿//FOR-Loop
+// DRY- principle- Do Not Repeat Yourself
+
+
+
+for (int i = 0; i < 5; i++) //i++ = i + 1
+{ 
+    Console.WriteLine("We don`t need no education.");
+}
+
+Console.WriteLine("Have a nice day.");
+
+//Console.WriteLine("We don`t need no education");
